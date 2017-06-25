3acf7601cbf5133d5d90e802765a93e9920da1d0
David Henley <henleydr@gmail.com>
2017-06-25 10:15:58 -0500

    Basic Redux App

---
+++ README.md
@@ -0,0 +1,5 @@
+# Redux App
+
+`npm install` or `yarn install`
+
+`yarn run start`
--- src/app.js
+++ src/app.js
@@ -1,1 +1,26 @@
-console.log('It works');
+import { createStore } from 'redux';
+
+const reducer = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state - 1;
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducer);
+
+store.subscribe(() => {
+  console.log('Current State:', store.getState());
+});
+
+store.dispatch({ type: 'NOTHING' });
+store.dispatch({ type: 'INCREMENT' });
+store.dispatch({ type: 'INCREMENT' });
+store.dispatch({ type: 'INCREMENT' });
+store.dispatch({ type: 'DECREMENT' });
+store.dispatch({ type: 'DECREMENT' });
+store.dispatch({ type: 'DECREMENT' });
